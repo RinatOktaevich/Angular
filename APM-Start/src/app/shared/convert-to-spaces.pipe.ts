7154a8a6d895dfd8e993890143f9e74fcde68ae5
Rinat Oktaevich <rinatoktaevich@gmail.com>
2018-08-01 18:36:36 +0300

    Save state

---
+++ APM-Start/src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from "../../../node_modules/@angular/core";
+
+@Pipe({
+    name: 'convertToSpaces'
+})
+
+export class ConvertToSpacesPipe implements PipeTransform {
+
+    transform(value:string,character:string):string{
+        return value.replace(character,' ');
+    }
+
+}
